2983dbc338aa6ba35692b731aebc91a96b796d9d
Yuripee <dttcar@gmail.com>
2023-09-24 11:49:43 +0200

    var declaration change

--- main.go
+++ main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func main() {
-	//set url
-	url := "https://api.baselinker.com/connector.php"
-	//add token
-	tok_val := "4005311-4011334-B6DI5PO6AM7GZ1D80O21R8W7OFFOH41W147FM3KTNHBJ9ZDHFLX0NONB1OZPWLXG"
-	//set method
+	var (
+		Url     string = "https://api.baselinker.com/connector.php"
+		Tok_val string = "4005311-4011334-B6DI5PO6AM7GZ1D80O21R8W7OFFOH41W147FM3KTNHBJ9ZDHFLX0NONB1OZPWLXG"
+	)
+
 	payload := []byte(`method=getInventories`)
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	req, err := http.NewRequest("POST", Url, bytes.NewBuffer(payload))
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("X-BLToken", tok_val)
+	req.Header.Set("X-BLToken", Tok_val)
 	req.Header.Set("Content-type", "application/x-www-form-urlencoded")
 
 	client := &http.Client{}
